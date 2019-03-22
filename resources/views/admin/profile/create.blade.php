4648a8ccccd20dd5139e7843dd46ed02cc84784d
hiroaki-kitagawa <hirock17l@icloud.com>
2019-03-22 14:37:09 +0000

    PHP/Laravel 10 課題

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MyProfile</title>
+</head>
+<body>
+    <h1>プロファイル作成画面</h1>
+</body>
+</html>
