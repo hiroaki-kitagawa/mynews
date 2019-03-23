26bd205d5bc6fdf72997ec375ab72b162e61db9d
hiroaki-kitagawa <hirock17l@icloud.com>
2019-03-23 18:01:41 +0000

    PHP/Laravel 13 課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
+                <h2>プロフィール編集</h2>
                 <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctyep="multipart/form-data">
                     
                     @if (count($errors) > 0)
