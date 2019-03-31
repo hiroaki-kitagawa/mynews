949b5b888c79251d2cc9054e9caaf835d71d191d
hiroaki-kitagawa <hirock17l@icloud.com>
2019-03-31 16:47:09 +0000

    uploadアクションをedit.blade.phpに記述

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -7,7 +7,7 @@
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール編集</h2>
                 <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-data">
-                    
+                    @csrf
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
