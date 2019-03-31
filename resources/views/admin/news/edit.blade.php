04f28022c6254ca90c27775697d933fbdc6ed699
hiroaki-kitagawa <hirock17l@icloud.com>
2019-03-31 16:49:30 +0000

    edit edit.blade.php

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -6,8 +6,7 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>ニュース編集</h2>
-                <!--<form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">-->
-                <form action="{{ action('NewsController@upload') }}" method="post" enctype="multipart/form-data">    
+                <form action="{{ action('Admin\NewsController@update') }}" method="post" enctype="multipart/form-data">
                     @csrf
                     @if (count($errors) > 0)
                         <ul>
